f90e79a593c86728534c1cdd4a3793aedd6fedfa
adewodu folashade <adewodufolashade@gmail.com>
2022-01-19 20:54:02 +0100

    update files

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,14 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+const name = "Tanya";
+const age = "10";
+console.log(name);
+
+let a = 5;
+const b = 10;
+const c = a + b;
+
+console.log(c);
+
+let d = 2 + 2;
+console.log(d);
